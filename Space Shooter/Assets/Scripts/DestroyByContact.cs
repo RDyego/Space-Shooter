1e78a298d0af13a088f27939a3613a934544d4e3
RDyego <dyego@alu.ufc.br>
2016-11-05 11:19:38 -0200

    Explosions

--- Space Shooter/Assets/Scripts/DestroyByContact.cs
+++ Space Shooter/Assets/Scripts/DestroyByContact.cs
@@ -3,13 +3,22 @@ using System.Collections;
 
 public class DestroyByContact : MonoBehaviour {
 
+    public GameObject explosion;
+    public GameObject playerExplosion;
+
 	private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Boundary"))
         {
             return;
         }
+        if (other.CompareTag("Player"))
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+        }
+        Instantiate(explosion, transform.position, transform.rotation);
         Destroy(other.gameObject);
         Destroy(gameObject);
     }
 }
+ 
