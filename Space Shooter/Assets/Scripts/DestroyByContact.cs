a3b14e01903b1b47aee445ee6b9a04d6f9889f5a
RDyego <dyego@alu.ufc.br>
2016-11-05 01:35:15 -0200

    Creating hazards

---
+++ Space Shooter/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour {
+
+	private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Boundary"))
+        {
+            return;
+        }
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
